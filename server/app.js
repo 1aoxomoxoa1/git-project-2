85d4596244d5206bdb0ce34139233b44f5d03f03
RomanBando <romanbando@gmail.com>
2023-01-04 13:55:55 +0500

    creating server

---
+++ server/app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const db = require("./db.js");
+const PORT = 4100;
+
+// express app
+const app = express();
+
+// connect to db
+db.authenticate().then(() => {
+  console.log('Connection has been established successfully.');
+}).catch((error) => {
+  console.error('Unable to connect to the database: ', error);
+});
+
+// waiting for requests
+app.listen(PORT);
---
+++ server/models/users.js
@@ -0,0 +1,31 @@
+const sequelize = require("../db.js");
+const { DataTypes } = require("sequelize");
+const Orders = require("./orders.js");
+
+const Users = sequelize.define("users", {
+  username: {
+    type: DataTypes.STRING(7),
+    primaryKey: true
+  },
+  email: {
+    type: DataTypes.STRING(100),
+    allowNull: false,
+    unique: true
+  },
+  phone: {
+    type: DataTypes.STRING(12),
+    allowNull: false,
+    unique: true
+  },
+  password: DataTypes.STRING(30),
+}, {
+  tableName: "users",
+  freezeTableName: true,
+  timestamps: false
+});
+
+Users.hasMany(Orders, {
+  foreignKey: 'username'
+})
+
+module.exports = Users;
